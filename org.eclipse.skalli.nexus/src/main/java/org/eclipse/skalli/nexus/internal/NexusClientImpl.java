aa1b001ee640ae1372687833e6f550ee1c1c2fd4
Britta Varwig <britta.varwig@sap.com>
2011-08-30 10:18:51 +0200

    encapsulate HttpException and IOException of the htttp request to nexus to add more details
    Change-Id: I3650b28c561d75867f8c8b733d8cdaf7ea83bf7b

--- org.eclipse.skalli.nexus/src/main/java/org/eclipse/skalli/nexus/internal/NexusClientImpl.java
+++ org.eclipse.skalli.nexus/src/main/java/org/eclipse/skalli/nexus/internal/NexusClientImpl.java
@@ -80,9 +80,18 @@ public class NexusClientImpl implements NexusClient {
         GetMethod method = new GetMethod(externalForm);
         method.setFollowRedirects(false);
         try {
-            LOG.info("GET " + nexusUrl); //$NON-NLS-1$
-            int statusCode = HttpUtils.getClient(nexusUrl).executeMethod(method);
-            LOG.info(statusCode + " " + HttpStatus.getStatusText(statusCode)); //$NON-NLS-1$
+            int statusCode;
+            try {
+                LOG.info("GET " + nexusUrl); //$NON-NLS-1$
+                statusCode = HttpUtils.getClient(nexusUrl).executeMethod(method);
+                LOG.info(statusCode + " " + HttpStatus.getStatusText(statusCode)); //$NON-NLS-1$
+            } catch (HttpException e) {
+                throw new HttpException(MessageFormat.format("Problems found for {0}: {1}", nexusUrl,
+                        e.getMessage()), e);
+            } catch (IOException e) {
+                throw new IOException(MessageFormat.format("Problems found for {0}: {1}", nexusUrl,
+                        e.getMessage()), e);
+            }
             if (statusCode == HttpStatus.SC_OK) {
                 InputStream in = method.getResponseBodyAsStream();
                 Document document;
